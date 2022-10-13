9c5a7222685e9e809a0c20adc7bfe4495598b76f
Brighton Code <brighton.m.daniels@gmail.com>
2022-10-13 17:15:05 -0400

    2008: Housing Bubble

--- Part2/US_DebtAdjustment/README.md
+++ Part2/US_DebtAdjustment/README.md
@@ -1,20 +1,36 @@
 # US Debt Crisis and Adjustment (2007-2001)
 ---
 
-## Emerging Bubble: 2004-2006
+# Emerging Bubble: 2004-2006
 ---
 
-### Early and Healthy Start of Debt Cycle: 
+> ## Classic Bubble Characteristics: 
+> 1. **Prices High** relative to traditional measures
+>     - flipping houses 
+> 2. Prices **Discounting Future Rapid Price Appreciation** from high levels
+> 3. **Broad Bullish Sentiment**
+>     - *Time* magazine cover "Will Your House Make You Rich?"
+> 4. **High Leverage** Purchases 
+>     - Leveraging up betting on prices continuing to increase 
+> 5. Buyers with **Extended Forward Purchases** to speculate/hedge future price gains
+>     - homebuilders stocking up supplies 
+>     - supplies increasing as high prices encouraged production 
+> 6. **New Entrants**
+>     - new single family homes doubled between 1995-2005
+> 7. **Stimulative Monetary Policy** to help inflate the bubble (tight pops)
+- Logic (opposite tends to happen in bubbles): 
+    - Betting on price changes? -> Deleverage or Sell
+    - Lenders become more cautious 
+- Everybody wanted to buy a house or lend to someone looking to buy a house
+
+### Early and Healthy Start of Debt Cycle "Goldilocks Period":
+- **Economy Strong, Income Moderate, Appreciating Asset Prices** 
 - Debt Grows in Line with Income Growth 
 - Debt Used to Finance Activities Growing Income 
 - Debt-to-GDP Ratio: Roughly Measures Balance 
     - 1990s Debt-to-GDP Ratio Increased Slightly (period of strong income growth and low unemployment)
     - 2001 Recession (Tightened Monetary Policy, dot-com bubble, 9/11 shock -> Fed Lower IR down to 1%)
 
-### Goldilocks Period: 
-- Economy Strong
-- Inflation Moderate
-- Appreciating Asset Prices 
 
 ### Interest Rate Movements: 
 - Federal Reserve focuses on **Growth, Inflation, and GDP Gap** (more than debt growth - don't regulate bubbles)
@@ -22,3 +38,31 @@
     - Debt Bubbles Usually Grow in One or More Individual Markets (easy to average out) 
 - Increased IR from lows of 1% to just over 5% in 2006 
 - Not enough to slow debt financed asset appreciation 
+
+## Housing Market Debt Bubble: 
+> #### 2004-2006: Home Prices Increased ~30%
+> #### 2000-2006: Home Prices Increased ~80%
+> #### Price rise clasically self-reinforcing in ways that create bubbles
+> #### Most houses bought with borrowed money (Mortgages)
+> - Home price gains have magnified impacts on equity values  
+> - Home value increases -> allow individual to borrow against their home equity 
+- Bubble Peak: ~8% of Households Buying a Home Each Year
+#### Household Debt Rose from 85% of Disposable Income in 2000 to 120% in 2006
+- Biggest increases in debt amongst lowest income earners 
+- Lowered credit standards made it easier for people to borrow against their home equity 
+- Home equity loans and cash-out refinancing totaled ~$500 billion in 2005
+    - 5x from 1998
+    - pushed overall US debt to over 300% GDP
+#### More Home Prices Went Up -> More Credit Standards Lowered 
+- Profitable for both lenders and borrowers 
+- Credit-fueled buying drove prices higher (self-reinforcing)
+- Leveraging Up took off in 2003-2007 
+- Banks not asking for proof of income before writing mortgage 
+    - Pushing adjustable rate mortgages (enticing borrowers with low "teaser" rates before rates increased later)
+    - Subprime Mortgages (riskier): 20% of the market 
+    - Mortgage Risk Packaged to Obscure Underlying Risks ("Securitization")
+        - Allowed for more avaliable credit + low interest rates 
+#### Fed's Easy Monetary Policy: 
+- Mortgage rates dropped to lows in 2003 (not seen since 1950s) 
+    - stayed near lows well into bubble
+- Rates rose ~1.5% in 2005-2007 (Still High Leveraging Up)
--- Part2/US_DebtAdjustment/timeline2008.py
+++ Part2/US_DebtAdjustment/timeline2008.py
@@ -8,6 +8,10 @@ from datetime import datetime, timedelta
 
 # https://datagy.io/pandas-add-row/
 # https://towardsdatascience.com/how-to-access-relational-databases-in-python-f711cb38e235
+# https://github.com/sukhbinder/timeline_in_python/blob/master/timeline_in_matplotlib.py
+# https://dadoverflow.com/2021/08/17/making-timelines-with-python/
+
+
 
 
 housing_crisis_timeline = pd.DataFrame.from_dict({ 
@@ -17,7 +21,16 @@ housing_crisis_timeline = pd.DataFrame.from_dict({
         '2004-10-20',
         '2005-04-28',
         '2005-05-10',
-        '2005-05-25'
+        '2005-05-25',
+        '2005-05-31',
+        '2005-07-09',
+        '2005-08-17',
+        '2005-08-28',
+        '2005-10-04',
+        '2006-01-08',
+        '2006-08-23',
+        '2006-08-24',
+        '2006-09-14'
         
         ],
     'event': [ 
@@ -26,7 +39,17 @@ housing_crisis_timeline = pd.DataFrame.from_dict({
         'Alan Greenspan claims growing mortgage debt is not a big burden',
         'Mortgage applications up due to decline in borrowing costs (purchase or refinance)',
         'U.S. Housing Bubble Appears',
-        'Steep Price Rises in Homes'
+        'Steep Price Rises in Homes',
+        'Fed Debates Pricking US Housing Bubble (asset prices out of Fed jurisdiction)',
+        'Real estate bringing in over 700,000 new jobs over past four years',
+        'Healthy Housing Market Lifted the Economy in July (prev. month)',
+        'Alan Greenspan predicts that the housing boom and consumer spending spree is near an end',
+        'Hot housing markets beginning to see a slowdown',
+        'Real estate investors warned that they would not expect repeated gains in 2005. Those who stayed course saw continued profits in the RE sector.',
+        'Housing market showing signs of potentially dragging down the economy.',
+        'Pre-owned home sales at their lowest level July 2006. Prices flattened and homes staying on the market longer.',
+        'Prime Adjustable-Rate Mortgages Foreclosing: Homeowners with good credit unable to pay their bills.'
+
 
         ],
 })
@@ -35,16 +58,43 @@ housing_crisis_timeline = pd.DataFrame.from_dict({
 federal_reserve_timeline = pd.DataFrame.from_dict({
     'date':[
         '2004-04-22',
-        '10-2004',
-        '11-2004'
+        '2004-10-20',
+        '2004-11-06',
+        '2005-05-31',
+        '2005-08-28',
+        '2006-02-01',
+        '2006-04-19',
+        '2006-07-10'
+        
         
         ],
     'event': [
-        'Alan Greenspan claims deflation threat is gone. Hints rising productivity will keep interest rates lower, longer.'
+        'Alan Greenspan claims deflation threat is gone. Hints rising productivity will keep interest rates lower, longer.',
         'Alan Greenspan claims growing mortgage debt is not a big burden.',
-        'When is accumulated debt unsustainable? Time to raise interest rates from rock bottom.'
+        'When is accumulated debt unsustainable? Time to raise interest rates from rock bottom.',
+        'Fed contemplating pricking US Housing Bubble (asset prices out of jurisdiction)',
+        'Greenspan predicts that the housing boom and consumer spending spree is near an end',
+        'Greenspan steps down after 18 years of service as Federal Reserve Chairman. Replaced by Ben Bernanke.',
+        'Fed signals policy shift on interest rates. Might stop campaign to raise IR next month (ST IR moved up 15x in the past 2 years)',
+        'Former Goldman Sachs Exec. Henry M. Paulson sworn in as Treasury Secretary.'
 
         ]
 
 })
 
+
+general_financial_news_timeline = pd.DataFrame.from_dict({
+    'date': [
+        '2005-12-17',
+        '2006-02-10',
+        '2006-07-10'
+
+    ],
+    'event': [
+        'Citigroup expanding existing retail business across the US',
+        'US Trade Deficit Record High in 2005. Up nearly 18% with a $725.8 billion gap (twice the 2001 deficit). Last surplus in 1975.',
+        'Former Goldman Sachs Exec. Henry M. Paulson sworn in as Treasury Secretary.'
+
+    ]
+
+})
