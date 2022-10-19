5fe40a960a49dc1e426ccd1ba2e24f2a64540e2a
Brighton Code <brighton.m.daniels@gmail.com>
2022-10-19 17:50:39 -0400

    2008 bubble intro plots complete

--- Part2/US_DebtAdjustment/cycles.py
+++ Part2/US_DebtAdjustment/cycles.py
@@ -258,8 +258,91 @@ class HOUSEHOLD_SPENDING():
 
         plt.show()
 
+class DEBT_FINANCED_ASSETS(): 
+    def __init__(self, start, end, frequency='q'): 
+        '''
+        Parameters: 
+        -----------
+        frequency: str
+        'd' = daily
+        'w' = weekly
+        'bw' = biweekly
+        'm' = monthly
+        'q' = quarterly
+        'sa' = semiannual
+        'a' = annual
+        start + end: datetime or datetime-like str
+        '''
+        self.start = start
+        self.end = end
+        self.frequency = frequency
+    
+    
+    def margin_excel(self): 
+        df = pd.DataFrame(pd.read_excel("margin-statistics.xlsx"))
+
+        df.index = df['Year-Month']
+        df.index = pd.to_datetime(df.index).strftime('%Y-%m-%d')
+        df = df.drop(['Year-Month'], axis=1)
+        
+        col_names = ['debit_bal_securities_margin', 'free_credit_bal_cash_acct', 'free_credit_bal_securities_margin']
+        df.columns = col_names
+        df = df.drop(['free_credit_bal_securities_margin'], axis=1)
+        
+        df.index.names = ['date']
+        df = df[(df.index > self.start) & (df.index < self.end)]
+        df = df.iloc[::-1]
+        
+
+        return df 
+    
+    def plot_margin(self): 
+        df = self.margin_excel()
+        df['debit_bal_growth'] = (df.debit_bal_securities_margin - df.debit_bal_securities_margin.shift(1,axis=0)) / df.debit_bal_securities_margin.shift(1,axis=0)
+        df['free_credit_growth'] = (df.free_credit_bal_cash_acct - df.free_credit_bal_cash_acct.shift(1,axis=0)) / df.free_credit_bal_cash_acct.shift(1,axis=0)
+
+        fig, ax = plt.subplots(figsize=(20,8))
+
+        ax.set_title("Margin Statistics During 2008", fontsize=25)
+        sns.barplot(x=df.index, y=df.debit_bal_growth, ax=ax, color='red', alpha=0.3, label='Debit Balance Growth')
+        sns.barplot(x=df.index, y=df.free_credit_growth, ax=ax, color='green', alpha=0.2, label='Credit Balance Growth')
+
+        ax.yaxis.set_major_formatter(PercentFormatter(xmax=1))
+        ax.set_ylabel("Percent Change in Balance")
+
+        ax.legend(loc=2)
+        ax.grid(False)
+        plt.xticks(rotation=50, fontsize=9)
 
 
+
+        ax2 = ax.twinx()
+        sns.lineplot(x=df.index, y=df.debit_bal_securities_margin, ax=ax2, label='Debit Balance')
+        sns.lineplot(x=df.index, y=df.free_credit_bal_cash_acct, ax=ax2, label='Free Credit Balance')
+
+        ax2.fill_between(x=df.index, y1=df.free_credit_bal_cash_acct, y2=df.debit_bal_securities_margin, where=df.free_credit_bal_cash_acct>df.debit_bal_securities_margin, label='Able to Meet Margin Call', alpha=0.5)
+        ax2.fill_between(x=df.index, y1=df.debit_bal_securities_margin, y2=df.free_credit_bal_cash_acct, where=df.debit_bal_securities_margin>df.free_credit_bal_cash_acct, color='red', alpha=0.5, label='Unable to Meet Margin Call')
+
+        ax2.set_ylabel("Millions", fontsize=15)
+
+        ax2.legend(loc=4)
+        ax2.annotate("Red zones show over-leveraged debt-financed asset appreciation", 
+                        xy = (0,-0.3), 
+                        xycoords = 'axes fraction', 
+                        ha = 'left', 
+                        va = 'center', 
+                        fontsize=15)
+        ax2.grid(False)
+        plt.xticks(rotation=50, fontsize=9)
+
+        plt.show()
+
+
+
+
+
+
+# S&P/Case-Shiller U.S. National Home Price Index (CSUSHPINSA) indexed to January 2000 at 100 (Not Seasonally Adjusted)
 
 print(f" 1990s: {start_early90} \n\
      LTCM Crash: {start_ltcm} \n\
